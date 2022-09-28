6be7229e862b0187a1883b2dbad757fbf85ffebc
Doug Hagan <dhagan@yahoo.com>
2022-09-28 08:33:54 -1000

    palindrome attempt

---
+++ leetcode/longestSubstring.js
@@ -0,0 +1,30 @@
+let s = "abavava"
+s = "fgew"
+
+const repeat = (s) => {
+    //let matched = [s[0]]
+
+    for (let index = a; index < s.length; index++) {
+        c = s[index]
+        if (s.indexOf(c) != s.lastIndexIf(c)) {
+            return false
+        }
+    }
+    return false
+}
+
+
+const longestSubstring = (s) => {
+    let result = s
+    for (let index = 0; index < s.length; index++) {   
+        for (let j = 0;  j < s.length; j++) {
+
+         
+        }   
+    }
+    return result 
+}
+
+
+//console.log(repeat(s))
+console.log(longestSubstring(s))
