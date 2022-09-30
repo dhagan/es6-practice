a08bbbe308a975adbf7ce751e6da4d74bc22920c
dhagan <dhagan@yahoo.com>
2022-09-30 12:02:31 -1000

    merge two ListNode

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -155,16 +155,57 @@ ll1 = new ListNode (1, ll2)
 
 
 
- var mergeTwoLists = function(list1, list2) {
+//  var mergeTwoLists = function(list1, list2) {
 
-    while (true) {
-        current = current.next
-        if (current.next == null) {
-            current.next = list2
-            return list1
-        } 
+//     c1 = list1
+//     c2 = list2
+//     while (true) {
+//         n1 = c1.next
+//         n2 = c2.next
+//         c1.next = c2
+//         c2.next = n1
+//         c1 = n1
+//         c2 = n2
+//         console.log(list1, list2)
+//         // next1 = list1.next
+//         // list1.next = list2
+//         // next2 = list2.next
+//         // list2.next = next1
+
+//         // current = current.next
+//         // if (current.next == null) {
+//         //     current.next = list2
+//         //     return list1
+//         // } 
+//     }
+    
+//};
+
+
+var mergeTwoLists = function(l1, l2) {
+    const dummy = new ListNode(0);
+    let current = dummy;  
+  
+    while(l1 !== null && l2 !== null){
+      if(l1.val <= l2.val){
+        current.next = l1;
+        l1 = l1.next;        
+      } else{
+        current.next = l2;
+        l2 = l2.next;        
+      }
+      current = current.next;
+    }
+  
+  	if(l1 !== null){
+      current.next = l1;        
+    }
+      
+    if(l2 !== null){
+      current.next = l2;
     }
     
+    return dummy.next;
 };
 
-console.log(mergeTwoLists(l3, ll3))
+console.log(mergeTwoLists(l1, ll1))
